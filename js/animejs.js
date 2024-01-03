018d08133e38a3969f5c62bd83e7ba8aa32ff09b
Swayam Kaushal <101446225+rajkaushal03@users.noreply.github.com>
2024-01-03 18:29:53 +0530

    Update animejs.js

--- js/animejs.js
+++ js/animejs.js
@@ -7,30 +7,6 @@ for(var i = 0; i<=100;i++)
     Blockcontainer.appendChild(blocks);
 }
 
-function animateBlocks()
-{
-    anime({
-        targets: '.block',
-        translateX: function(){
-            return anime.random(-1000,1000);
-        },
-        translateY: function () {
-            return anime.random(-500,500);
-        },
-        scale: function () {
-            return anime.random(1,5);
-        },
-
-        easing: 'linear',
-        duration:3000,
-        delay:anime.stagger(10),
-        complete:animateBlocks,
-    })
-}
-
-animateBlocks()
-
-
 
 function animateBlocks()
 {
